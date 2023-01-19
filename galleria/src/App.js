22861a48a9451cdff5f7913832c70966cb1b537c
Silas  Owu <silas.owu@amalitech.com>
2023-01-19 15:59:12 -0800

    Homepage and Slideshow activeness done

--- galleria/src/App.js
+++ galleria/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Homepage from './component/homepage';
-
+import {Routes, Route} from "react-router-dom"
+import Slideshow from './component/slideshow';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='Slideshow' element={<Slideshow/>}/>
+    </Routes>
   );
 }
 
