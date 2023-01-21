16197cce5d666725af39b5fa9a3d2f1621f808c1
Gloria Gyamfi <gloria.gyamfi@amalitech.com>
2023-01-20 16:49:36 -0800

    i worked on the gallery

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Homepage from './component/homepage';
 import {Routes, Route} from "react-router-dom"
 import Slideshow from './component/slideshow';
+import Gallery from './component/Gallery/Gallery';
 
 function App() {
   return (
-    <Routes>
-        <Route path='/' element={<Homepage/>} />
-        <Route path='Slideshow' element={<Slideshow/>}/>
-    </Routes>
+    // <Routes>
+    //     <Route path='/' element={<Homepage/>} />
+    //     <Route path='Slideshow' element={<Slideshow/>}/>
+    // </Routes>
+    <>
+      <Gallery />
+    </>
   );
 }
 
---
+++ src/component/Gallery/Gallery.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles.css'
+
+function Gallery() {
+  return (
+        <div className='background'>
+            <div className='pop-up'>
+                <span>CLOSE</span>
+                <div className='image'>
+                    <img src='./assets/starry-night/hero-large.jpg' alt='' />
+                </div>
+            </div>
+        </div>
+  )
+}
+
+<div className='backgroundgi'>
+            <div className='pop-up_1'>
+                <span>CLOSE</span>
+                <div className='image'>
+                    <img src='./assets/girl-with-pearl-earring/hero-large.jpg' alt='' />
+                </div>
+            </div>
+        </div>
+
+export default Gallery
