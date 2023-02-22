3a9796163a1d325b0eb345bfadcb3eaa49d2ec8d
Edmund Gardener <edmund.gardener@amalitech.com>
2023-02-22 15:01:54 +0000

    mobile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {Routes, Route} from "react-router-dom"
 import Slideshow from './component/Slideshow/Slideshow';
 import Homepage from './component/Homepage';
+// import Homepage from './component/Homepage';
 // import Gallery from './component/Gallery/Gallery';
 
 function App() {
   return (
     <Routes>
-        <Route path='/' element={<Homepage/>} />
+        <Route path='/' element={<Homepage />} />
         <Route path='Slideshow' element={<Slideshow/>}/>
     </Routes>
   );
